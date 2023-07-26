be0fe93d2093cfd283076dabeb3016c6798971cc
GulnaraChe <123952717+GulnaraChe@users.noreply.github.com>
2023-07-26 21:32:54 +0500

    Update StatisticsService.java

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,15 +1,6 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
-    /*        public long findMax(long[] incomes) {
-                long currentMax = incomes[0];
-                for (long income : incomes) {
-                    if (currentMax < income) {
-                        currentMax = income;
-                    }
-                }
-                return currentMax;
-            }*/
     public long findMax(long[] incomes) {
         long maxValue = incomes[0];
         for (int i = 0; i < incomes.length; i++) {
